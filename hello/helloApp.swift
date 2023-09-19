7cb1ec09d93950b2cfb425134806262fd8487aef
wonny1012 <rnjswldnjs1012@gmail.com>
2023-09-19 09:35:25 +0900

    Initial Commit

---
+++ hello/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by kwon ji won on 2023/09/19.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
